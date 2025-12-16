c1e56d991b940b499294ef3ebcb9b1b96d073262
francaparroz21 <francaparroz21@gmail.com>
2025-12-16 18:43:38 +0100

    we add Hero component, inside we have the text of the hero component and a phrase (flipword)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
     return (
@@ -8,7 +9,7 @@ const App = () => {
             { /* Navbar */}
             <Navbar />
             { /* Home */}
-            
+            <Hero />
             { /* Navbar */}
 
             { /* Navbar */}
---
+++ src/components/HeroText.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { FlipWords } from './FlipWords'
+
+const HeroText = () => {
+  return (
+    <div className='z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text '>
+
+      {/*Desktop */}
+      <div className='flex-col hidden md:flex c-space'>
+        <h1 className='text-4xl font-medium'>
+          Hi, My name is Francisco!
+        </h1>
+        <div className='flex flex-col items-start'>
+          <p className='text-5xl font-medium text-neutral-300'>A developer <br />dedicated to thechnology and music.</p>
+          <div className='text-4xl font-medium text-neutral-300'>
+            <FlipWords className='font-black text-white text-8xl' words={['Web solutions.']} />
+          </div>
+
+        </div>
+      </div>
+
+
+
+      {/*Mobile */}
+    </div>
+  )
+}
+
+export default HeroText
